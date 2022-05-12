5dedd2b2ab3bd6c86ce8531ebb50c81ac14e752a
Sinkevich Svetlana <sinkevich.svetlana89@gmail.com>
2022-05-12 22:57:28 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import img from './img/flag-ukraine_1f1fa-1f1e6.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Hello World!
+        </h1>
+      <img src={img} className="flag" alt="flag" />
       </header>
     </div>
   );
